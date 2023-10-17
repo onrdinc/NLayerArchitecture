80d0f1f245258cf02a0edeb50afa818facdb98a2
Onur DİNÇ <103895148+onrdinc@users.noreply.github.com>
2023-10-17 17:57:38 +0300

    .

--- NLayerApp/Business/Implementations/BankBs.cs
+++ NLayerApp/Business/Implementations/BankBs.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Business.Interfaces;
 using DataAccess.Interfaces;
+using Infrastructure.UnitOfWorks.Interface;
 using Infrastructure.Utilites.ApiResponses;
 using Models.Dtos;
 using Models.Entities;
@@ -16,15 +17,48 @@ namespace Business.Implementations
     {
         private readonly IBankRepository _repo;
         private readonly IMapper _mapper;
-        public BankBs(IBankRepository repo,IMapper mapper)
+        private IUnitOfWork _unitOfWork;
+        public BankBs(IBankRepository repo,IMapper mapper,IUnitOfWork unitOfWork)
         {
             _repo = repo;   
             _mapper = mapper;
+            _unitOfWork = unitOfWork;
         }
 
-        public Task<ApiResponse<BankDto.Response>> Add(BankDto.Form form, int currentUserId)
+        public async Task<ApiResponse<BankDto.Response>> Add(BankDto.Form form, int currentUserId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                ApiResponse<BankDto.Response> rb = new ApiResponse<BankDto.Response>();
+
+
+
+                if (string.IsNullOrEmpty(form.Name))
+                {
+                    rb.StatusCode = 400;
+                    rb.StatusTexts.Add("Kategori adı boş geçilemez.");
+                    return await Task.FromResult(rb);
+                }
+
+                var categori = _mapper.Map<Banks>(form);
+                categori.CreatedDate = DateTime.Now;
+                categori.CreatedBy = currentUserId;
+                var insert = await _repo.AddAsync(categori);
+                await _unitOfWork.CommitAsync();
+                var c = _mapper.Map<BankDto.Response>(insert);
+
+                //rb = SingleGet(c.Id, currentUserId).Result;
+
+                rb.StatusCode = 200;
+                rb.StatusTexts.Add("Kaydedildi.");
+                return await Task.FromResult(rb);
+                //return ApiResponse<MRoleGroupTypeDto.Response>.Success(StatusCodes.Status201Created, c);
+            }
+            catch (Exception ex)
+            {
+
+                throw await Task.FromResult(ex);
+            }
         }
 
         public Task<ApiResponse<NoData>> Delete(int id, int currentUserId)
